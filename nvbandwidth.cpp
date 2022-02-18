adfefd60cd44b0a2706ba692c4f7326921cffb75
Bill Fiser <wfiser@nvidia.com>
2022-02-17 16:09:57 -0800

    specify CUDA Driver

--- nvbandwidth.cpp
+++ nvbandwidth.cpp
@@ -140,7 +140,7 @@ int main(int argc, char **argv) {
 
     int cudaVersion;
     CU_ASSERT(cuDriverGetVersion(&cudaVersion));
-    std::cout << "Using CUDA Version: " << cudaVersion << std::endl;
+    std::cout << "Using CUDA Driver Version: " << cudaVersion << std::endl;
 
     char driverVersion[NVML_SYSTEM_DRIVER_VERSION_BUFFER_SIZE];
     NVML_ASSERT(nvmlSystemGetDriverVersion(driverVersion, NVML_SYSTEM_DRIVER_VERSION_BUFFER_SIZE));
